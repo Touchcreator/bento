52baee07bee8cba9da03b65f7fc547b9c3ed1fee
TouchIsNotAGoodCoder <64277067+Touchcreator@users.noreply.github.com>
2025-09-26 18:31:24 -0400

    Update config.js

--- config.js
+++ config.js
@@ -77,9 +77,9 @@ const CONFIG = {
 		},
 		{
 			id: '5',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
+			name: 'Google Classroom',
+			icon: 'notebook-text',
+			link: 'https://classroom.google.com',
 		},
 		{
 			id: '6',
